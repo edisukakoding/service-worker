007733569666eb3a20fa92563c5ccf447d20b9a4
Edi Hartono <96169046+edisukakoding@users.noreply.github.com>
2023-01-03 04:22:29 +0000

    add offline

--- sw.js
+++ sw.js
@@ -4,12 +4,16 @@ this.addEventListener('install', event => {
         '/service-worker/index.html',
         '/service-worker/app.js',
         '/service-worker/style.css',
+        '/service-worker/offline.html',
     ])))
 })
 
 this.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request).catch(() => fetch(event.request))).then((r) => {
-        caches.open('v1').then(cache => cache.put(event.request, r))
-        return r.clone()
-    }).catch(() => caches.match('/service-worker/index.html'))
+    event.respondWith(caches.match(event.request)
+        .catch(() => fetch(event.request))
+        .then(r => {
+            caches.open('v1').then(cache => cache.put(event.request, r))
+            return r.clone();
+        })
+        .catch(() => caches.match('/service-worker/offline.html')))
 })
