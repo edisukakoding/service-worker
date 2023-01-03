f9e178c2eebaffc149c66158ea0115cfa9f597e8
Edi Hartono <96169046+edisukakoding@users.noreply.github.com>
2023-01-03 03:15:35 +0000

    tes

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+alert('ok')
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker/sw.js').then(function(reg) {
+        console.log('service worker registered', reg)
+    }).catch(function(err) {
+        console.log('service worker registration failed', err)
+    })
+}
