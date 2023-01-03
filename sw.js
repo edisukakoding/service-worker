9fa2dc973e4e5950fea6e6525b2f5c8d72048791
Edi Hartono <96169046+edisukakoding@users.noreply.github.com>
2023-01-03 03:55:11 +0000

    add service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+this.addEventListener('install', event => {
+    event.waitUnit(caches.open('v1').then(cache => cache.addAll([
+        '/service-worker/',
+        '/service-worker/index.html',
+        '/service-worker/app.js',
+        '/service-worker/style.css',
+    ])))
+})
 
+this.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).catch(() => fetch(event.request))).then((r) => {
+        caches.open('v1').then(cache => cache.put(event.request, r))
+    })
+    return r.clone()
+}).catch(() => caches.match('/service-worker/index.html'))
